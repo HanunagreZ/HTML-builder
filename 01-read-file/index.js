690eb4c2726a14870da24182c1ebf535aab22b85
HanunagreZ <hanunagrez@yandex.ru>
2024-01-17 10:31:51 +0300

    feat: solve first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const txtPath = path.resolve(__dirname, './text.txt');
+
+const readStream = fs.createReadStream(txtPath);
+readStream.on('readable', () => {
+  console.log(`Inner text: ${readStream.read()}`);
+});
+readStream.on('end', () => {
+  console.log('End of file.');
+});
